b6e66a36372a870263a12c3586863221f8dccca5
Konstantin Aksenov <kav.pbg@gmail.com>
2024-01-13 21:31:55 +0100

    START 08 - Refactoring

--- models/world.class.js
+++ models/world.class.js
@@ -80,6 +80,13 @@ class World {
 
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
 
+    // Red rectangle
+    this.ctx.beginPath();
+    this.ctx.lineWidth = "2";
+    this.ctx.strokeStyle = "red";
+    this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
+    this.ctx.stroke();
+
     if (mo.otherDirection) {
       mo.x = mo.x * -1;
       this.ctx.restore();
