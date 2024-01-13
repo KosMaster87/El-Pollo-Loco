56bee7bda3614827065a8682278e2d0cf3c6c4ba
Konstantin Aksenov <kav.pbg@gmail.com>
2024-01-14 00:50:21 +0100

    START 11 - Collision detection

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -99,7 +99,6 @@ class MovableObject {
       this.Y + this.offsetY + this.height >= obj.Y &&
       this.Y + this.offsetY <= obj.Y + obj.height &&
       obj.onCollisionCourse
-
       // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir.
       // Nützlich bei Gegenständen, auf denen man stehen kann.
     );
