5cb6df8192bb1e1ca3443883c03285911a361d1c
Konstantin <kav.pbg@gmail.com>
2024-02-17 18:34:01 -0300

    join weiter machen

--- models/cloud.clas.js
+++ models/cloud.clas.js
@@ -5,10 +5,17 @@ class Cloud extends MovableObject {
   width = 500;
   height = 300;
   speed = 0.15;
+  IMAGES_CLAUD = [
+    "img/5_background/layers/4_clouds/1.png",
+    "img/5_background/layers/4_clouds/2.png",
+    "img/5_background/layers/4_clouds/1.png",
+    "img/5_background/layers/4_clouds/2.png",
+  ];
 
   constructor() {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
 
+    this.loadImages(this.IMAGES_CLAUD);
     // Start the cloud in random place.
     this.x = Math.random() * 500;
     this.animate();
